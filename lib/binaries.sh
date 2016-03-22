c8b96e400348f4f311d1167e883114ea3cccedab
Pranjal Kumar <pranjal.kumar@appdynamics.com>
2016-03-22 10:43:10 +0530

    Update binaries.sh

--- lib/binaries.sh
+++ lib/binaries.sh
@@ -12,15 +12,13 @@ install_nodejs() {
   local dir="$2"
 
   if needs_resolution "$version"; then
-    BP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
-    versions_as_json=$(ruby -e "require 'yaml'; print YAML.load_file('$BP_DIR/manifest.yml')['dependencies'].select {|dep| dep['name'] == 'node' }.map {|dep| dep['version']}")
-    stable_version='0.12.11'
-    version=$($BP_DIR/bin/node $BP_DIR/lib/version_resolver.js "$version" "$versions_as_json" "$stable_version")
+    echo "Resolving node version ${version:-(latest stable)} via semver.io..."
+    version=$($BP_DIR/bin/node $BP_DIR/lib/version_resolver.js "$version")
   fi
 
   echo "Downloading and installing node $version..."
-  local download_url="https://s3pository.heroku.com/node/v$version/node-v$version-$os-$cpu.tar.gz"
-  curl "`translate_dependency_url $download_url`" --silent --fail --retry 5 --retry-max-time 15 -o /tmp/node.tar.gz || (>&2 $BP_DIR/compile-extensions/bin/recommend_dependency $download_url && false)
+  local download_url="http://s3pository.heroku.com/node/v$version/node-v$version-$os-$cpu.tar.gz"
+  curl "`translate_dependency_url $download_url`" --silent --fail -o /tmp/node.tar.gz || (>&2 $BP_DIR/compile-extensions/bin/recommend_dependency $download_url && false)
   echo "Downloaded [`translate_dependency_url $download_url`]"
   tar xzf /tmp/node.tar.gz -C /tmp
   rm -rf $dir/*
@@ -34,12 +32,12 @@ install_iojs() {
 
   if needs_resolution "$version"; then
     echo "Resolving iojs version ${version:-(latest stable)} via semver.io..."
-    version=$(curl --silent --get  --retry 5 --retry-max-time 15 --data-urlencode "range=${version}" https://semver.herokuapp.com/iojs/resolve)
+    version=$(curl --silent --get --data-urlencode "range=${version}" https://semver.herokuapp.com/iojs/resolve)
   fi
 
   echo "Downloading and installing iojs $version..."
   local download_url="https://iojs.org/dist/v$version/iojs-v$version-$os-$cpu.tar.gz"
-  curl "$download_url" --silent --fail --retry 5 --retry-max-time 15 -o /tmp/node.tar.gz || (echo "Unable to download iojs $version; does it exist?" && false)
+  curl "$download_url" --silent --fail -o /tmp/node.tar.gz || (echo "Unable to download iojs $version; does it exist?" && false)
   tar xzf /tmp/node.tar.gz -C /tmp
   mv /tmp/iojs-v$version-$os-$cpu/* $dir
   chmod +x $dir/bin/*
@@ -53,7 +51,7 @@ install_npm() {
   else
     if needs_resolution "$version"; then
       echo "Resolving npm version ${version} via semver.io..."
-      version=$(curl --silent --get --retry 5 --retry-max-time 15 --data-urlencode "range=${version}" https://semver.herokuapp.com/npm/resolve)
+      version=$(curl --silent --get --data-urlencode "range=${version}" https://semver.herokuapp.com/npm/resolve)
     fi
     if [[ `npm --version` == "$version" ]]; then
       echo "npm `npm --version` already installed with node"
@@ -63,7 +61,6 @@ install_npm() {
     fi
   fi
 }
-
 
 install_appdynamics() {
   local build_dir=${1:-}
