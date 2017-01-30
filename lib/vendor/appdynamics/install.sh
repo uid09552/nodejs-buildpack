c471747c9bbfc5325bff447d5fcda3572735e3be
Nikhil Katre <nkatre@users.noreply.github.com>
2017-01-30 13:18:18 -0800

    NodeName should be auto-populated
    NodeName should be auto-populated

--- lib/vendor/appdynamics/install.sh
+++ lib/vendor/appdynamics/install.sh
@@ -26,7 +26,7 @@ then
   APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY=$(echo "${VCAP_SERVICES-}" | $JQ --raw-output '.['\""$key"\"'][0] | .credentials | .["account-access-key"] ')
   APPDYNAMICS_AGENT_APPLICATION_NAME=$(echo "${VCAP_APPLICATION-}" | $JQ --raw-output .application_name)
   APPDYNAMICS_AGENT_TIER_NAME=$(echo "${VCAP_APPLICATION-}" | $JQ --raw-output .application_name)
-  APPDYNAMICS_AGENT_NODE_NAME=$(echo "${VCAP_APPLICATION-}" | $JQ --raw-output .application_name)
+  #APPDYNAMICS_AGENT_NODE_NAME=$(echo "${VCAP_APPLICATION-}" | $JQ --raw-output .application_name)
 
   if [ ! -z "${APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY-}" ];
   then
@@ -54,8 +54,8 @@ then
     if [ ! -z "${APPDYNAMICS_AGENT_TIER_NAME-}" ]; then
       echo "export APPDYNAMICS_AGENT_TIER_NAME=$APPDYNAMICS_AGENT_TIER_NAME" >> $SETUP_APPDYNAMICS
     fi
-    if [ ! -z "${APPDYNAMICS_AGENT_NODE_NAME-}" ]; then
-      echo "export APPDYNAMICS_AGENT_NODE_NAME=$APPDYNAMICS_AGENT_NODE_NAME" >> $SETUP_APPDYNAMICS
-    fi
+    #if [ ! -z "${APPDYNAMICS_AGENT_NODE_NAME-}" ]; then
+    #  echo "export APPDYNAMICS_AGENT_NODE_NAME=$APPDYNAMICS_AGENT_NODE_NAME" >> $SETUP_APPDYNAMICS
+    #fi
   fi
 fi
